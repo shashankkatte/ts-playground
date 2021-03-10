a05a5ebd9817f04b20b420d113a660ed4f28c8a7
shashankkatte <shashankanand.katte@gmail.com>
2021-03-10 11:02:49 -0500

    src : Using interfaces tocacth errors at development

--- 01_fetchjson/index.ts
+++ 01_fetchjson/index.ts
@@ -7,8 +7,31 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+//  Intefaces help define structure of objects in Typescript
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  //   const todo = response.data;
+
+  //  Type script way of doing
+  const todo = response.data as Todo;
+
+  // This 3 lines print out undefined only after execution if we use plain JS. But with TS these line shout at you saying something is wrong as we are using interfaces.
+  //   const ID = todo.ID;
+  //   const title = todo.Title;
+  //   const finished = todo.finished;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+  The Todo with ID : ${ID}
+   Has a title of: ${title}
+   Is it finished? ${finished}
+  `);
 });
-
-
