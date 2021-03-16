6428ebe7a1c40e4a1b8d804cf57ca016d0cae62b
shashankkatte <shashankanand.katte@gmail.com>
2021-03-16 10:15:12 -0400

    src : Removing duplicate logic: initial solution

--- 01_sample_apps/02_maps/src/index.ts
+++ 01_sample_apps/02_maps/src/index.ts
@@ -13,5 +13,8 @@ const user = new User();
 const company = new Company();
 const customMap = new CustomMap('map');
 
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+// customMap.addUserMarker(user);
+// customMap.addCompanyMarker(company);
+
+customMap.addMarker(user);
+customMap.addMarker(company);
