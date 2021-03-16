b0bce3359c8de13fa6924526dea047a30859ff07
shashankkatte <shashankanand.katte@gmail.com>
2021-03-16 06:14:57 -0400

    src : Using type def files to generate random users from faker

--- 01_sample_apps/02_maps/src/User.ts
+++ 01_sample_apps/02_maps/src/User.ts
@@ -8,5 +8,11 @@ class User {
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
