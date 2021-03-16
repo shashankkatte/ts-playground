350e80c50f0769a7bc67ca6acd39297268c12134
shashankkatte <shashankanand.katte@gmail.com>
2021-03-16 07:31:12 -0400

    src : showing up basic map on html

--- 01_sample_apps/02_maps/src/index.ts
+++ 01_sample_apps/02_maps/src/index.ts
@@ -1,10 +1,16 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log(user);
-console.log(company);
+// console.log(user);
+// console.log(company);
 
-google
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
