763424fbd35a031afd19e28a42e74d61a5e907b7
shashankkatte <shashankanand.katte@gmail.com>
2021-03-16 05:57:17 -0400

    src : Add Faker for random data

--- 01_sample_apps/02_maps/src/User.ts
+++ 01_sample_apps/02_maps/src/User.ts
@@ -1,1 +1,12 @@
-class User {}
+// Import statements in TS look identical to ES2015
+import faker from 'faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {}
+}
